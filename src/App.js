2b19824ecb0e12f9f9efae3cefbc912b6a9e69c1
Marti Pena <penabernilsmarti@gmail.com>
2021-01-14 10:53:57 +0100

    Search bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import {
   BrowserRouter as Router,
@@ -10,6 +10,24 @@ import {
 } from "react-router-dom";
 
 function App() {
+
+  const [query, setQuery] = useState('');
+
+  const searchMovies = async (e) => {
+    e.preventDefault();
+
+    //const query="hola";
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=f37c16e288bd47f8c2026f6fdc704e57&language=en-US&query=${query}`;
+    //const url = "https://api.themoviedb.org/3/trending/movie/week?api_key=f37c16e288bd47f8c2026f6fdc704e57&page=1";
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+    } catch (er) {
+      console.log(er);
+    }
+
+  }
 
   return (
 
@@ -22,24 +40,27 @@ function App() {
           Trending Movies
         </a>
 
+          <form className="form-inline" onSubmit={searchMovies}>
 
-          <form className="form-inline">
-            <input class="form-control mr-sm-2 navbar-form" type="search" placeholder="Search a movie" aria-label="Search" id="movieSearch"></input>
-            <Link to="/movieSearch">
-              <input class="btn btn-outline-info my-2 my-sm-0 navbar-form searchButton" type="button" value="Search"></input>
+            <input class="form-control mr-sm-2 navbar-form" type="search" aria-label="Search" id="movieSearch" name="query" value={query} onChange={(e) => setQuery(e.target.value)} placeholder="Type here bruh"></input>
+            <Link to={'/movieSearch/' + query}>
+              <button className="button" type="submit">Search</button>
             </Link>
+
           </form>
 
         </nav>
 
         <Route exact path="/" component={MovieList} />
-        <Route exact path="/movieSearch" component={MovieSearch} />
+        <Route exact path="/movieSearch/:query" component={MovieSearch} />
         <Route exact path="/movie/:movieID" component={MovieDetails} />
       </Router>
     </div>
 
   );
 }
+
+
 
 class MovieList extends React.Component {
   constructor() {
@@ -87,7 +108,6 @@ class MovieList extends React.Component {
       for (let i = 1; i <= 5; i++) {
         pageNumbers.push(i);
       }
-
 
       renderPageNumbers = pageNumbers.map(number => {
         let classes = this.state.page === number ? 'activePage' : 'pages';
@@ -151,15 +171,16 @@ class Movie extends React.Component {
     super();
   }
 
+
   render() {
     let info = this.props.movie;
-    console.log('https://image.tmdb.org/t/p/w500' + info.backdrop_path);
-    
-    
+    //console.log('https://image.tmdb.org/t/p/w500' + info.backdrop_path);
+
+
     return (
 
       <div className="col mb-4">
-        <Link to={'/movie/'+info.id}>
+        <Link to={'/movie/' + info.id}>
           <div className="card bgCard" id={info.id}>
             <img src={'https://image.tmdb.org/t/p/w500' + info.backdrop_path} className="card-img-top" alt={info.original_title}></img>
             <div className="card-body">
@@ -203,7 +224,7 @@ class MovieDetails extends React.Component {
   }*/
 
   componentDidMount() {
-    let url = "https://api.themoviedb.org/3/movie/"+this.state.idFilm+"?api_key=f37c16e288bd47f8c2026f6fdc704e57";
+    let url = "https://api.themoviedb.org/3/movie/" + this.state.idFilm + "?api_key=f37c16e288bd47f8c2026f6fdc704e57";
     console.log(url);
     console.log(this.state.idFilm);
     fetch(url)
@@ -222,7 +243,7 @@ class MovieDetails extends React.Component {
   render() {
     return (
       <div>
-        
+
 
         <div class="row featurette mt-5 px-3">
           <div class="col-md-8 order-md-2">
@@ -243,19 +264,118 @@ class MovieSearch extends React.Component {
   constructor(props) {
     super();
     this.state = {
-      img: []
+      movies: [],
+      page: 1,
+
     }
+    
   }
 
+  componentDidMount() {
+    this.makeHttpRequestWithPage(1);
+    /*fetch("https://api.themoviedb.org/3/trending/movie/week?api_key=f37c16e288bd47f8c2026f6fdc704e57&page=${pageNumber}")
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          movies: json.results,
+          page: json.page
+        });
+      }); */
+  }
 
+  makeHttpRequestWithPage = async pageNumber => {
+    if (pageNumber <= 1) {
+      pageNumber = 1;
+    }
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=f37c16e288bd47f8c2026f6fdc704e57&language=en-US&query=${this.props.match.params.query}&page=` + pageNumber)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          movies: json.results,
+          page: json.page,
+
+        });
+      });
+      
+  }
   render() {
+    
+    let renderPageNumbers;
+    const pageNumbers = [];
+    if (this.state.total !== null) {
+      for (let i = 1; i <= 5; i++) {
+        pageNumbers.push(i);
+      }
+
+      renderPageNumbers = pageNumbers.map(number => {
+        let classes = this.state.page === number ? 'activePage' : 'pages';
+
+        return (
+          <li class="page-item">
+            <a className={classes} onClick={() => this.makeHttpRequestWithPage(number)}>
+              <span key={number}>{number}</span>
+            </a>
+          </li>
+        );
+      });
+    }
+    /*<div className="row row-cols-1 row-cols-md-3 p-3">{this.state.movies.map((film, idx) =>
+            <Movie key={idx} movie={film}></Movie>
+          )}
+          </div>
+          onClick={this.filterMovie}
+          <div className="row row-cols-1 row-cols-md-3 p-3">{this.state.movies.filter(movie=>movie.original_title.includes(this.state.filter|| movie.title.includes(this.state.filter))).map((film, idx) =>
+            <Movie key={idx} movie={film}></Movie>
+          )}
+          </div>*/
     return (
       <div>
-        <p>Hola :)</p>
 
+        <div className="cosPagina">
+          <div className="row row-cols-1 row-cols-md-5 p-3">{this.state.movies.map((film, idx) =>
+            <Movie key={idx} movie={film}></Movie>
+          )}
+          </div>
+          <ul class="pagination centerPagination">
+            <li class="page-item">
+              <a class="page-link pages" onClick={() => this.makeHttpRequestWithPage((this.state.page - 1))} aria-label="Previous">
+                <span aria-hidden="true">«</span>
+              </a>
+            </li>
+
+            {renderPageNumbers}
+
+            <li class="page-item">
+              <a class="page-link pages" aria-label="Next" onClick={() => this.makeHttpRequestWithPage((this.state.page + 1))}>
+                <span aria-hidden="true">»</span>
+              </a>
+            </li>
+          </ul>
+
+        </div>
+
+        <footer className="footer mt-auto py-3 bg-dark">
+          <div className="container">
+            <span className="text-muted footerText">Àlex Burdoy, Josep Vílchez i Martí Peña</span>
+          </div>
+        </footer>
       </div>
     );
   }
+
+
+  /*render() {
+    console.log(this.props.match.params.query);
+    return (
+
+      <div className="col mb-4">
+
+
+
+      </div>
+
+    );
+  }*/
 }
 
 
